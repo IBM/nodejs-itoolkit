42fad0e581bce9f76f41cfa5e41cc615237bd562
mengxumx <mengxumx@cn.ibm.com>
2018-09-17 05:07:08 -0500

    rewrite to be compliance with ES6

--- test/qsh.js
+++ test/qsh.js
@@ -1,11 +1,11 @@
 
-var assert = require('assert');
-var NodeVer = process.version.slice(1,2);
+const assert = require('assert');
+const NodeVer = process.version.slice(1,2);
 assert.notEqual(NodeVer, '0', 'Unsupported version of Node.js!');
-var xt = require('../lib/itoolkit');
-var hint = 'check the "success" property in return value'
+const xt = require('../lib/itoolkit');
+const hint = 'check the "success" property in return value'
 
-var opt = {
+const opt = {
   xslib : 'XMLSERVICE',
   db   : '*LOCAL',
   user : 'USER',
@@ -15,13 +15,13 @@ var opt = {
   //path : '/cgi-bin/xmlcgi.pgm'
 };
 
-var conn = new xt.iConn(opt.db,null,null,opt);
+let conn = new xt.iConn(opt.db,null,null,opt);
 conn.debug(true);
 conn.add(xt.iQsh('system wrksyssts'));
-conn.run(function(str){
-  var results = xt.xmlToJson(str);
-  var success = true;
-  results.every(function(result, i){
+conn.run( (str) => {
+  let results = xt.xmlToJson(str);
+  let success = true;
+  results.every( (result, i) => {
     if(result.hasOwnProperty('success'))
       success = result.success == true;
   });
--- test/test.js
+++ test/test.js
@@ -12,13 +12,13 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
 // IN THE SOFTWARE.
 
-var assert = require('assert');
-var NodeVer = process.version.slice(1,2);
+const assert = require('assert');
+const NodeVer = process.version.slice(1,2);
 assert.notEqual(NodeVer, '0', 'Unsupported version of Node.js!');
-var xt = require('../lib/itoolkit');
-var hint = 'check the "success" property in return value'
+const xt = require('../lib/itoolkit');
+const hint = 'check the "success" property in return value'
 //Need change based on your server configurations
-var opt = {
+const opt = {
   db   : '*LOCAL',
   user : 'YOURNAME',
   pwd  : 'PASSWORD',
@@ -27,16 +27,15 @@ var opt = {
 	path : '/cgi-bin/xmlcgi.pgm'
 };
 
-describe('Basic Function Test', function() {
-  this.timeout(5000)
-  describe('Test iCmd()', function() {
-    it(hint, function(done) {
-      var conn = new xt.iConn(opt.db);
+describe('Basic Function Test', () => {
+  describe('Test iCmd()', () => {
+    it(hint, (done) => {
+      let conn = new xt.iConn(opt.db);
       conn.add(xt.iCmd('RTVJOBA USRLIBL(?) SYSLIBL(?)'));
-      conn.run(function(str){
-        var results = xt.xmlToJson(str);
-        var success = true;
-        results.every(function(result, i){
+      conn.run((str) => {
+        let results = xt.xmlToJson(str);
+        let success = true;
+        results.every((result, i) => {
           if(result.hasOwnProperty('success'))
             success = result.success == true;
         });
@@ -46,14 +45,14 @@ describe('Basic Function Test', function() {
     });
   });
   
-  describe('Test iSh()', function() {
-    it(hint, function(done) {
-      var conn = new xt.iConn(opt.db);
+  describe('Test iSh()', () => {
+    it(hint, (done) => {
+      let conn = new xt.iConn(opt.db);
       conn.add(xt.iSh('system -i wrksyssts'));
-      conn.run(function(str){
-        var results = xt.xmlToJson(str);
-        var success = true;
-        results.every(function(result, i){
+      conn.run((str) => {
+        let results = xt.xmlToJson(str);
+        let success = true;
+        results.every((result, i) => {
           if(result.hasOwnProperty('success'))
             success = result.success == true;
         });
@@ -63,14 +62,14 @@ describe('Basic Function Test', function() {
     });
   });
   
-  describe('Test iQsh()', function() {
-    it(hint, function(done) {
-      var conn = new xt.iConn(opt.db);
+  describe('Test iQsh()', () => {
+    it(hint, (done) => {
+      let conn = new xt.iConn(opt.db);
       conn.add(xt.iQsh('system wrksyssts'));
-      conn.run(function(str){
-        var results = xt.xmlToJson(str);
-        var success = true;
-        results.every(function(result, i){
+      conn.run((str) => {
+        let results = xt.xmlToJson(str);
+        let success = true;
+        results.every((result, i) => {
           if(result.hasOwnProperty('success'))
             success = result.success == true;
         });
@@ -80,11 +79,11 @@ describe('Basic Function Test', function() {
     });
   });
   
-  describe('Test iPgm()', function() {
-    it(hint, function(done) {
-      var conn = new xt.iConn(opt.db);
-      var pgm = new xt.iPgm('QWCRSVAL', {'lib':'QSYS'});
-      var outBuf = [
+  describe('Test iPgm()', () => {
+    it(hint, (done) => {
+      let conn = new xt.iConn(opt.db);
+      let pgm = new xt.iPgm('QWCRSVAL', {'lib':'QSYS'});
+      let outBuf = [
           [0, '10i0'],
           [0, '10i0'],
           ['', '36h'],
@@ -100,10 +99,10 @@ describe('Basic Function Test', function() {
       pgm.addParam('QCCSID', '10A');
       pgm.addParam(this.errno, {'io':'both', 'len' : 'rec2'});
       conn.add(pgm);
-      conn.run(function(str){
-        var results = xt.xmlToJson(str);
-        var success = false;
-        results.every(function(result, i){
+      conn.run((str) => {
+        let results = xt.xmlToJson(str);
+        let success = false;
+        results.every((result, i) => {
           if(result.hasOwnProperty('success'))
             success = result.success == true;
         });
@@ -111,10 +110,10 @@ describe('Basic Function Test', function() {
         else done(new Error(JSON.stringify(results)));
       });
     });
-    it('Should return arbitrarily named parameter', function(done) {
-      var conn = new xt.iConn(opt.db);
-      var pgm = new xt.iPgm('QWCRSVAL', {'lib':'QSYS'});
-      var outBuf = [
+    it('Should return arbitrarily named parameter', (done) => {
+      let conn = new xt.iConn(opt.db);
+      let pgm = new xt.iPgm('QWCRSVAL', {'lib':'QSYS'});
+      let outBuf = [
           [0, '10i0'],
           [0, '10i0'],
           ['', '36h'],
@@ -128,13 +127,13 @@ describe('Basic Function Test', function() {
       pgm.addParam(66, '10i0');
       pgm.addParam(1, '10i0');
       pgm.addParam('QCCSID', '10A');
-      var paramValue = 'errno';
+      let paramValue = 'errno';
       pgm.addParam(this.errno, {'io':'both', 'len' : 'rec2', 'name' : paramValue });
       conn.add(pgm);
-      conn.run(function(str){
-        var results = xt.xmlToJson(str);
-        var success = false;
-        results.every(function(result, i){
+      conn.run((str) => {
+        let results = xt.xmlToJson(str);
+        let success = false;
+        results.every((result, i) => {
           if(result.data[11].hasOwnProperty('name'))
             success = result.data[11].name == paramValue;
         });
@@ -142,34 +141,34 @@ describe('Basic Function Test', function() {
         else done(new Error(JSON.stringify(results)));
       });
     });
-    it('Should return success with addReturn arbitrary attribute specified', function(done) {
-      var conn = new xt.iConn(opt.db);
-      var pgm = new xt.iPgm("ZZSRV6", {"lib":"XMLSERVICE", "func":"ZZVARY4"});
-      pgm.addParam("Gill", "10A", {"varying":"4"});
-      var test_value = "NEW_NAME";
-      pgm.addReturn("0", "20A", {"varying":"4","name":test_value});
+    it('Should return success with addReturn arbitrary attribute specified', (done) => {
+      let conn = new xt.iConn(opt.db);
+      let pgm = new xt.iPgm("ZZSRV6", {"lib":"XMLSERVICE", "func":"ZZVARY4"});
+      pgm.addParam("Gill", "10A", {"letying":"4"});
+      let test_value = "NEW_NAME";
+      pgm.addReturn("0", "20A", {"letying":"4","name":test_value});
       conn.add(pgm);
-      conn.run(function(str) {
-        var results = xt.xmlToJson(str);
+      conn.run((str) => {
+        let results = xt.xmlToJson(str);
         if(results[0].data[1].name == test_value) done();
         else done(new Error(JSON.stringify(results)));
       });      
     });
   });
   
-  describe('Test iSql()', function() {
-    it(hint, function(done) {
-      var conn = new xt.iConn(opt.db);
-      var sql = new xt.iSql();  /* Test iSql Class */
+  describe('Test iSql()', () => {
+    it(hint, (done) => {
+      let conn = new xt.iConn(opt.db);
+      let sql = new xt.iSql();  /* Test iSql Class */
       sql.prepare('call qsys2.tcpip_info()');
       sql.execute();
       sql.fetch();
       sql.free();
       conn.add(sql);
-      conn.run(function(str){
-        var results = xt.xmlToJson(str);
-        var success = true;
-        results.every(function(result, i){
+      conn.run((str) => {
+        let results = xt.xmlToJson(str);
+        let success = true;
+        results.every((result, i) => {
           if(result.hasOwnProperty('success'))
             success = result.success == true;
         });
@@ -178,21 +177,21 @@ describe('Basic Function Test', function() {
       });
     });
     
-    it('should return SQL result set', function(done) {
-      var conn = new xt.iConn(opt.db);
-      var sql = new xt.iSql();
+    it('should return SQL result set', (done) => {
+      let conn = new xt.iConn(opt.db);
+      let sql = new xt.iSql();
       sql.addQuery("SELECT LSTNAM, STATE FROM QIWS.QCUSTCDT");
       sql.fetch();
       sql.free();
       conn.add(sql);
-      conn.run(function(str){
-        var results = xt.xmlToJson(str);
-        var success1 = false;
-        var success2 = false;
-        results.every(function(result, i){
+      conn.run((str) => {
+        let results = xt.xmlToJson(str);
+        let success1 = false;
+        let success2 = false;
+        results.every((result, i) => {
           if(result.hasOwnProperty('success'))
             success1 = result.success == true;
-          result.result.every(function(row, i){
+          result.result.every((row, i) => {
             success2 = row[0].hasOwnProperty('desc');
           })
         });
@@ -201,21 +200,21 @@ describe('Basic Function Test', function() {
       });
     });
 
-    it('should parse SQL result set empty data tags correctly', function(done) {
-      var conn = new xt.iConn(opt.db);
-      var sql = new xt.iSql();
+    it('should parse SQL result set empty data tags correctly', (done) => {
+      let conn = new xt.iConn(opt.db);
+      let sql = new xt.iSql();
       sql.addQuery("SELECT '' AS BLANK, STATE FROM QIWS.QCUSTCDT");
       sql.fetch();
       sql.free();
       conn.add(sql);
-      conn.run(function(str){
-        var results = xt.xmlToJson(str);
-        var success1 = false;
-        var success2 = false;
-        results.every(function(result, i){
+      conn.run((str) => {
+        let results = xt.xmlToJson(str);
+        let success1 = false;
+        let success2 = false;
+        results.every((result, i) => {
           if(result.hasOwnProperty('success'))
             success1 = result.success == true;
-          result.result.every(function(row, i){
+          result.result.every((row, i) => {
             success2 = row[0].value === '';
           })
         });
