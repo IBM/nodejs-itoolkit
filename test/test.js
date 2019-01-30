a73d03151ba2ce8272a00ba0b65b1e24b28529ff
Mark Irish <mirish@ibm.com>
2019-01-30 12:12:31 -0600

    Linted the tests, and made them use assert (before they only failed if the last object tested were not successful, and tracked success with a variable). Renamed the sql class to SqlCall. General cleanup

--- test/test.js
+++ test/test.js
@@ -1,147 +1,150 @@
-// Copyright (c) International Business Machines Corp. 2017
+// Copyright (c) International Business Machines Corp. 2019
 // All Rights Reserved
 
-// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
-// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
-// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+// associated documentation files (the "Software"), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify, merge, publish, distribute,
+// sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
 
-// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
-// IN THE SOFTWARE.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+// NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+/* eslint-env mocha */
 
 const assert = require('assert');
 const xt = require('../lib/itoolkit');
-const hint = 'check the "success" property in return value'
-//Need change based on your server configurations
+
+const hint = 'check the "success" property in return value';
+// Need change based on your server configurations
 const opt = {
-  db   : '*LOCAL',
-  user : 'YOURNAME',
-  pwd  : 'PASSWORD',
-	host : '0.0.0.0',
-	port : 8080,
-	path : '/cgi-bin/xmlcgi.pgm'
+  db: '*LOCAL',
+  user: 'YOURNAME',
+  pwd: 'PASSWORD',
+  host: '0.0.0.0',
+  port: 8080,
+  path: '/cgi-bin/xmlcgi.pgm',
 };
 
 describe('Basic Function Test', () => {
   describe('Test iCmd()', () => {
     it(hint, (done) => {
-      let conn = new xt.iConn(opt.db);
+      const conn = new xt.Connection(opt.db);
       conn.add(xt.iCmd('RTVJOBA USRLIBL(?) SYSLIBL(?)'));
       conn.run((str) => {
-        let results = xt.xmlToJson(str);
-        let success = true;
-        results.every((result, i) => {
-          if(result.hasOwnProperty('success'))
-            success = result.success == true;
+        const results = xt.xmlToJson(str);
+        results.forEach((result) => {
+          if (Object.prototype.hasOwnProperty.call(result, 'success')) { 
+            assert(result.success === true);
+          }
         });
-        if(success) done();
-        else done(new Error(JSON.stringify(results)));
+        done();
       });
     });
   });
-  
+
   describe('Test iSh()', () => {
     it(hint, (done) => {
-      let conn = new xt.iConn(opt.db);
+      const conn = new xt.Connection(opt.db);
       conn.add(xt.iSh('system -i wrksyssts'));
       conn.run((str) => {
-        let results = xt.xmlToJson(str);
-        let success = true;
-        results.every((result, i) => {
-          if(result.hasOwnProperty('success'))
-            success = result.success == true;
+        const results = xt.xmlToJson(str);
+        results.forEach((result) => {
+          if (Object.prototype.hasOwnProperty.call(result, 'success')) {
+            assert(result.success === true);
+          }
         });
-        if(success) done();
-        else done(new Error(JSON.stringify(results)));
+        done();
       });
     });
   });
-  
+
   describe('Test iQsh()', () => {
     it(hint, (done) => {
-      let conn = new xt.iConn(opt.db);
+      const conn = new xt.Connection(opt.db);
       conn.add(xt.iQsh('system wrksyssts'));
       conn.run((str) => {
-        let results = xt.xmlToJson(str);
-        let success = true;
-        results.every((result, i) => {
-          if(result.hasOwnProperty('success'))
-            success = result.success == true;
+        const results = xt.xmlToJson(str);
+        results.forEach((result) => {
+          if (Object.prototype.hasOwnProperty.call(result, 'success')) {
+            assert(result.success === true);
+          }
         });
-        if(success) done();
-        else done(new Error(JSON.stringify(results)));
+        done();
       });
     });
   });
-  
+
   describe('Test iPgm()', () => {
     it(hint, (done) => {
-      let conn = new xt.iConn(opt.db);
-      let pgm = new xt.iPgm('QWCRSVAL', {'lib':'QSYS'});
-      let outBuf = [
-          [0, '10i0'],
-          [0, '10i0'],
-          ['', '36h'],
-          ['', '10A'],
-          ['', '1A'],
-          ['', '1A'],
-          [0, '10i0'],
-          [0, '10i0']
-        ];
-      pgm.addParam(outBuf, {'io':'out'});
+      const conn = new xt.Connection(opt.db);
+      const pgm = new xt.ProgramCall('QWCRSVAL', { lib: 'QSYS' });
+      const outBuf = [
+        [0, '10i0'],
+        [0, '10i0'],
+        ['', '36h'],
+        ['', '10A'],
+        ['', '1A'],
+        ['', '1A'],
+        [0, '10i0'],
+        [0, '10i0'],
+      ];
+      pgm.addParam(outBuf, { io: 'out' });
       pgm.addParam(66, '10i0');
       pgm.addParam(1, '10i0');
       pgm.addParam('QCCSID', '10A');
-      pgm.addParam(this.errno, {'io':'both', 'len' : 'rec2'});
+      pgm.addParam(this.errno, { io: 'both', len: 'rec2' });
       conn.add(pgm);
       conn.run((str) => {
-        let results = xt.xmlToJson(str);
-        let success = false;
-        results.every((result, i) => {
-          if(result.hasOwnProperty('success'))
-            success = result.success == true;
+        const results = xt.xmlToJson(str);
+        results.forEach((result) => {
+          if (Object.prototype.hasOwnProperty.call(result, 'success')) {
+            assert(result.success === true);
+          }
         });
-        if(success) done();
-        else done(new Error(JSON.stringify(results)));
+        done();
       });
     });
+
     it('Should return arbitrarily named parameter', (done) => {
-      let conn = new xt.iConn(opt.db);
-      let pgm = new xt.iPgm('QWCRSVAL', {'lib':'QSYS'});
-      let outBuf = [
-          [0, '10i0'],
-          [0, '10i0'],
-          ['', '36h'],
-          ['', '10A'],
-          ['', '1A'],
-          ['', '1A'],
-          [0, '10i0'],
-          [0, '10i0']
-        ];
-      pgm.addParam(outBuf, {'io':'out'});
+      const conn = new xt.Connection(opt.db);
+      const pgm = new xt.ProgramCall('QWCRSVAL', { lib: 'QSYS' });
+      const outBuf = [
+        [0, '10i0'],
+        [0, '10i0'],
+        ['', '36h'],
+        ['', '10A'],
+        ['', '1A'],
+        ['', '1A'],
+        [0, '10i0'],
+        [0, '10i0'],
+      ];
+      pgm.addParam(outBuf, { io: 'out' });
       pgm.addParam(66, '10i0');
       pgm.addParam(1, '10i0');
       pgm.addParam('QCCSID', '10A');
-      let paramValue = 'errno';
-      pgm.addParam(this.errno, {'io':'both', 'len' : 'rec2', 'name' : paramValue });
+      const paramValue = 'errno';
+      pgm.addParam(this.errno, { io: 'both', len: 'rec2', name: paramValue });
       conn.add(pgm);
       conn.run((str) => {
-        let results = xt.xmlToJson(str);
-        let success = false;
-        results.every((result, i) => {
-          if(result.data[11].hasOwnProperty('name'))
-            success = result.data[11].name == paramValue;
+        const results = xt.xmlToJson(str);
+        results.forEach((result) => {
+          if (Object.prototype.hasOwnProperty.call(result.data[11], 'name')) {
+            assert(result.data[11].name === paramValue);
+          }
         });
-        if(success) done();
-        else done(new Error(JSON.stringify(results)));
+        done();
       });
     });
-    /*** Refer to test/rpg/zzsrv6.rpgle
+    /** * Refer to test/rpg/zzsrv6.rpgle
     it('Should return success with addReturn arbitrary attribute specified', (done) => {
-      let conn = new xt.iConn(opt.db);
+      let conn = new xt.Connection(opt.db);
       let pgm = new xt.iPgm("ZZSRV6", {"lib":"XMLSERVICE", "func":"ZZVARY4"});
       pgm.addParam("Gill", "10A", {"letying":"4"});
       let test_value = "NEW_NAME";
@@ -149,77 +152,72 @@ describe('Basic Function Test', () => {
       conn.add(pgm);
       conn.run((str) => {
         let results = xt.xmlToJson(str);
-        if(results[0].data[1].name == test_value) done();
+        if(results[0].data[1].name === test_value) done();
         else done(new Error(JSON.stringify(results)));
-      });      
+      });
     });
-    ***/
+    ** */
   });
-  
+
   describe('Test iSql()', () => {
     it(hint, (done) => {
-      let conn = new xt.iConn(opt.db);
-      let sql = new xt.iSql();  /* Test iSql Class */
+      const conn = new xt.Connection(opt.db);
+      const sql = new xt.SqlCall(); /* Test iSql Class */
       sql.prepare('call qsys2.tcpip_info()');
       sql.execute();
       sql.fetch();
       sql.free();
       conn.add(sql);
       conn.run((str) => {
-        let results = xt.xmlToJson(str);
-        let success = true;
-        results.every((result, i) => {
-          if(result.hasOwnProperty('success'))
-            success = result.success == true;
+        const results = xt.xmlToJson(str);
+        results.forEach((result) => {
+          if (Object.prototype.hasOwnProperty.call(result, 'success')) {
+            assert(result.success === true);
+          }
         });
-        if(success) done();
-        else done(new Error(JSON.stringify(results)));
+        done();
       });
     });
-    
+
     it('should return SQL result set', (done) => {
-      let conn = new xt.iConn(opt.db);
-      let sql = new xt.iSql();
-      sql.addQuery("SELECT LSTNAM, STATE FROM QIWS.QCUSTCDT");
+      const conn = new xt.Connection(opt.db);
+      const sql = new xt.SqlCall();
+      sql.addQuery('SELECT LSTNAM, STATE FROM QIWS.QCUSTCDT');
       sql.fetch();
       sql.free();
       conn.add(sql);
       conn.run((str) => {
-        let results = xt.xmlToJson(str);
-        let success1 = false;
-        let success2 = false;
-        results.every((result, i) => {
-          if(result.hasOwnProperty('success'))
-            success1 = result.success == true;
-          result.result.every((row, i) => {
-            success2 = row[0].hasOwnProperty('desc');
-          })
+        const results = xt.xmlToJson(str);
+        results.forEach((result) => {
+          if (Object.prototype.hasOwnProperty.call(result, 'success')) {
+            assert(result.success === true);
+          }
+          result.result.forEach((row) => {
+            assert(Object.prototype.hasOwnProperty.call(row[0], 'desc'));
+          });
         });
-        if(success1 && success2) done();
-        else done(new Error(JSON.stringify(results)));
+        done();
       });
     });
 
     it('should parse SQL result set empty data tags correctly', (done) => {
-      let conn = new xt.iConn(opt.db);
-      let sql = new xt.iSql();
+      const conn = new xt.Connection(opt.db);
+      const sql = new xt.SqlCall();
       sql.addQuery("SELECT '' AS BLANK, STATE FROM QIWS.QCUSTCDT");
       sql.fetch();
       sql.free();
       conn.add(sql);
       conn.run((str) => {
-        let results = xt.xmlToJson(str);
-        let success1 = false;
-        let success2 = false;
-        results.every((result, i) => {
-          if(result.hasOwnProperty('success'))
-            success1 = result.success == true;
-          result.result.every((row, i) => {
-            success2 = row[0].value === '';
-          })
+        const results = xt.xmlToJson(str);
+        results.forEach((result) => {
+          if (Object.prototype.hasOwnProperty.call(result, 'success')) {
+            assert(result.success === true);
+          }
+          result.result.forEach((row) => {
+            assert(row[0].value === '');
+          });
         });
-        if(success1 && success2) done();
-        else done(new Error(JSON.stringify(results)));
+        done();
       });
     });
   });
