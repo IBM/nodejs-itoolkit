56f019db1570f04648fab5e56d82ba32901d2c58
Dave Weissman <weissmad@gmail.com>
2019-03-02 18:06:28 -0500

    fix spacing
    Signed-off-by: Dave Weissman <weissmad@gmail.com>

--- test/unit/iPgmUnit.js
+++ test/unit/iPgmUnit.js
@@ -74,122 +74,118 @@ describe('iPgm Class Unit Tests', () => {
       pgm.addParam(outBuf, { io: 'out' });
 
       let expectedXML = '<pgm name=\'QTOCNETSTS\' lib=\'QSYS\' func=\'QtoRtvTCPA\' error=\'fast\'>'
-      + '<parm io=\'out\'><ds><data type=\'10i0\'>0</data><data type=\'10i0\'>'
-      + '0</data><data type=\'36h\'></data><data type=\'10A\'></data>'
-      + '<data type=\'1A\'></data><data type=\'1A\'></data><data type=\'10i0\'>'
-      + '0</data><data type=\'10i0\'>0</data></ds></parm></pgm>';
+        + '<parm io=\'out\'><ds><data type=\'10i0\'>0</data><data type=\'10i0\'>'
+        + '0</data><data type=\'36h\'></data><data type=\'10A\'></data>'
+        + '<data type=\'1A\'></data><data type=\'1A\'></data><data type=\'10i0\'>'
+        + '0</data><data type=\'10i0\'>0</data></ds></parm></pgm>';
 
       expect(pgm.toXML()).to.equal(expectedXML);
 
       pgm.addParam(66, '10i0');
 
       expectedXML = '<pgm name=\'QTOCNETSTS\' lib=\'QSYS\' func=\'QtoRtvTCPA\' error=\'fast\'>'
-      + '<parm io=\'out\'><ds><data type=\'10i0\'>0</data><data type=\'10i0\'>'
-      + '0</data><data type=\'36h\'></data><data type=\'10A\'></data>'
-      + '<data type=\'1A\'></data><data type=\'1A\'></data><data type=\'10i0\'>'
-      + '0</data><data type=\'10i0\'>0</data></ds></parm>'
-      + '<parm><data type=\'10i0\'>66</data></parm></pgm>';
+        + '<parm io=\'out\'><ds><data type=\'10i0\'>0</data><data type=\'10i0\'>'
+        + '0</data><data type=\'36h\'></data><data type=\'10A\'></data>'
+        + '<data type=\'1A\'></data><data type=\'1A\'></data><data type=\'10i0\'>'
+        + '0</data><data type=\'10i0\'>0</data></ds></parm>'
+        + '<parm><data type=\'10i0\'>66</data></parm></pgm>';
 
       expect(pgm.toXML()).to.equal(expectedXML);
 
       pgm.addParam(1, '10i0');
 
       expectedXML = '<pgm name=\'QTOCNETSTS\' lib=\'QSYS\' func=\'QtoRtvTCPA\' error=\'fast\'>'
-      + '<parm io=\'out\'><ds><data type=\'10i0\'>0</data><data type=\'10i0\'>'
-      + '0</data><data type=\'36h\'></data><data type=\'10A\'></data>'
-      + '<data type=\'1A\'></data><data type=\'1A\'></data><data type=\'10i0\'>'
-      + '0</data><data type=\'10i0\'>0</data></ds></parm>'
-      + '<parm><data type=\'10i0\'>66</data></parm>'
-      + '<parm><data type=\'10i0\'>1</data></parm></pgm>';
+        + '<parm io=\'out\'><ds><data type=\'10i0\'>0</data><data type=\'10i0\'>'
+        + '0</data><data type=\'36h\'></data><data type=\'10A\'></data>'
+        + '<data type=\'1A\'></data><data type=\'1A\'></data><data type=\'10i0\'>'
+        + '0</data><data type=\'10i0\'>0</data></ds></parm>'
+        + '<parm><data type=\'10i0\'>66</data></parm>'
+        + '<parm><data type=\'10i0\'>1</data></parm></pgm>';
 
       expect(pgm.toXML()).to.equal(expectedXML);
 
       pgm.addParam('QCCSID', '10A');
 
       expectedXML = '<pgm name=\'QTOCNETSTS\' lib=\'QSYS\' func=\'QtoRtvTCPA\' error=\'fast\'>'
-      + '<parm io=\'out\'><ds><data type=\'10i0\'>0</data><data type=\'10i0\'>'
-      + '0</data><data type=\'36h\'></data><data type=\'10A\'></data>'
-      + '<data type=\'1A\'></data><data type=\'1A\'></data><data type=\'10i0\'>'
-      + '0</data><data type=\'10i0\'>0</data></ds></parm>'
-      + '<parm><data type=\'10i0\'>66</data></parm>'
-      + '<parm><data type=\'10i0\'>1</data></parm>'
-      + '<parm><data type=\'10A\'>QCCSID</data></parm></pgm>';
+        + '<parm io=\'out\'><ds><data type=\'10i0\'>0</data><data type=\'10i0\'>'
+        + '0</data><data type=\'36h\'></data><data type=\'10A\'></data>'
+        + '<data type=\'1A\'></data><data type=\'1A\'></data><data type=\'10i0\'>'
+        + '0</data><data type=\'10i0\'>0</data></ds></parm>'
+        + '<parm><data type=\'10i0\'>66</data></parm>'
+        + '<parm><data type=\'10i0\'>1</data></parm>'
+        + '<parm><data type=\'10A\'>QCCSID</data></parm></pgm>';
 
       expect(pgm.toXML()).to.equal(expectedXML);
 
       pgm.addParam(errno, { io: 'both', len: 'rec2' });
 
       expectedXML = '<pgm name=\'QTOCNETSTS\' lib=\'QSYS\' func=\'QtoRtvTCPA\' error=\'fast\'>'
-      + '<parm io=\'out\'><ds><data type=\'10i0\'>0</data><data type=\'10i0\'>'
-      + '0</data><data type=\'36h\'></data><data type=\'10A\'></data>'
-      + '<data type=\'1A\'></data><data type=\'1A\'></data><data type=\'10i0\'>'
-      + '0</data><data type=\'10i0\'>0</data></ds></parm>'
-      + '<parm><data type=\'10i0\'>66</data></parm>'
-      + '<parm><data type=\'10i0\'>1</data></parm>'
-      + '<parm><data type=\'10A\'>QCCSID</data></parm>'
-      + '<parm io=\'both\'><ds len=\'rec2\'><data type=\'10i0\'>0</data>'
-      + '<data type=\'10i0\' setlen=\'rec2\'>0</data><data type=\'7A\'>'
-      + '</data><data type=\'1A\'></data></ds></parm></pgm>';
+        + '<parm io=\'out\'><ds><data type=\'10i0\'>0</data><data type=\'10i0\'>'
+        + '0</data><data type=\'36h\'></data><data type=\'10A\'></data>'
+        + '<data type=\'1A\'></data><data type=\'1A\'></data><data type=\'10i0\'>'
+        + '0</data><data type=\'10i0\'>0</data></ds></parm>'
+        + '<parm><data type=\'10i0\'>66</data></parm>'
+        + '<parm><data type=\'10i0\'>1</data></parm>'
+        + '<parm><data type=\'10A\'>QCCSID</data></parm>'
+        + '<parm io=\'both\'><ds len=\'rec2\'><data type=\'10i0\'>0</data>'
+        + '<data type=\'10i0\' setlen=\'rec2\'>0</data><data type=\'7A\'>'
+        + '</data><data type=\'1A\'></data></ds></parm></pgm>';
 
       expect(pgm.toXML()).to.equal(expectedXML);
     });
-<<<<<<< HEAD
 
     it('regular <parm> contains by=\'val\'', () => {
-      const pgm = new iPgm("MYPGM", {lib:"MYLIB", func: "MY_PROCEDURE"});
+      const pgm = new iPgm("MYPGM", { lib: "MYLIB", func: "MY_PROCEDURE" });
 
-      pgm.addParam("", "1A", {by:"val"});
-      pgm.addReturn("", "2A", {name:"output"});
+      pgm.addParam("", "1A", { by: "val" });
+      pgm.addReturn("", "2A", { name: "output" });
 
-      let lookAtXML=pgm.toXML();
+      let lookAtXML = pgm.toXML();
       expect(lookAtXML).to.match(/<parm .*by='val'.*>/);
-  });
+    });
 
-  it('data structure <parm> contains by=\'val\'', () => {
-      const pgm = new iPgm("MYPGM", {lib:"MYLIB", func: "MY_PROCEDURE"});
+    it('data structure <parm> contains by=\'val\'', () => {
+      const pgm = new iPgm("MYPGM", { lib: "MYLIB", func: "MY_PROCEDURE" });
 
-      const p_inds=[
+      const p_inds = [
         [0, "3s0"],
-        [0, "7s0", {name:"ds_fld2"}]
+        [0, "7s0", { name: "ds_fld2" }]
       ];
-      
-      pgm.addParam(p_inds, {name:"inds", by:"val"});
-      pgm.addReturn("", "2A", {name:"output"});
-      
-      let lookAtXML=pgm.toXML();
+
+      pgm.addParam(p_inds, { name: "inds", by: "val" });
+      pgm.addReturn("", "2A", { name: "output" });
+
+      let lookAtXML = pgm.toXML();
       expect(lookAtXML).to.match(/<parm .*by='val'.*>/);
-  });
+    });
 
-  it('regular <parm> contains by=\'val\', with io=\'both\'', () => {
-      const pgm = new iPgm("MYPGM", {lib:"MYLIB", func: "MY_PROCEDURE"});
+    it('regular <parm> contains by=\'val\', with io=\'both\'', () => {
+      const pgm = new iPgm("MYPGM", { lib: "MYLIB", func: "MY_PROCEDURE" });
 
-      pgm.addParam("", "1A", {by:"val", io:"both"});
-      pgm.addReturn("", "2A", {name:"output"});
+      pgm.addParam("", "1A", { by: "val", io: "both" });
+      pgm.addReturn("", "2A", { name: "output" });
 
-      let lookAtXML=pgm.toXML();
+      let lookAtXML = pgm.toXML();
       expect(lookAtXML).to.match(/<parm .*by='val'.*>/);
       expect(lookAtXML).to.match(/<parm .*io='both'.*>/);
-  });
+    });
 
-  it('data structure <parm> contains by=\'val\', with io=\'both\'', () => {
-      const pgm = new iPgm("MYPGM", {lib:"MYLIB", func: "MY_PROCEDURE"});
+    it('data structure <parm> contains by=\'val\', with io=\'both\'', () => {
+      const pgm = new iPgm("MYPGM", { lib: "MYLIB", func: "MY_PROCEDURE" });
 
-      const p_inds=[
+      const p_inds = [
         [0, "3s0"],
-        [0, "7s0", {name:"ds_fld2"}]
+        [0, "7s0", { name: "ds_fld2" }]
       ];
-      
-      pgm.addParam(p_inds, {name:"inds", by:"val", io:"both"});
-      pgm.addReturn("", "2A", {name:"output"});
-      
-      let lookAtXML=pgm.toXML();
+
+      pgm.addParam(p_inds, { name: "inds", by: "val", io: "both" });
+      pgm.addReturn("", "2A", { name: "output" });
+
+      let lookAtXML = pgm.toXML();
       expect(lookAtXML).to.match(/<parm .*by='val'.*>/);
       expect(lookAtXML).to.match(/<parm .*io='both'.*>/);
+    });
   });
-=======
->>>>>>> Added unit and functional tests
-  });
-
 
   describe('addReturn', () => {
     it('appends return to pgm xml', () => {
@@ -203,7 +199,7 @@ describe('iPgm Class Unit Tests', () => {
       pgm.addReturn('0', '20A');
 
       const expectedXML = '<pgm name=\'QTOCNETSTS\' lib=\'QSYS\' func=\'QtoRtvTCPA\' '
-      + 'error=\'fast\'><return><data type=\'20A\'>0</data></return></pgm>';
+        + 'error=\'fast\'><return><data type=\'20A\'>0</data></return></pgm>';
 
       expect(pgm.toXML()).to.equal(expectedXML);
     });
