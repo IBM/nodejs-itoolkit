80fa6db14cd27795ac0c10a89c9be55aba6ae575
Abdirahim Musse <33973272+abmusse@users.noreply.github.com>
2019-03-18 18:17:49 -0500

    Move existing unit/functional tests to deprecated folder
    Functional tests using deprecated class moved to:
    - test/functional/deprecated
    
    Unit tests using deprecated classes moved to:
      - test/unit/deprecated
    
    This was done to keep pre v1.0 tests around to ensure compatability.
    
    At some point we will stop supporting these in the future.
    
    Then we can simply git rm these two folders

---
+++ test/functional/deprecated/iDataQueueFunctional.js
@@ -0,0 +1,134 @@
+// Copyright (c) International Business Machines Corp. 2019
+// All Rights Reserved
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+// associated documentation files (the "Software"), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify, merge, publish, distribute,
+// sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+// NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+/* eslint-env mocha */
+/* eslint-disable new-cap */
+
+const { expect } = require('chai');
+const { iConn, iDataQueue } = require('../../../lib/itoolkit');
+
+// Set Env variables or set values here.
+const opt = {
+  database: process.env.TKDB || '*LOCAL',
+  username: process.env.TKUSER || '',
+  password: process.env.TKPASS || '',
+  host: process.env.TKHOST || 'localhost',
+  port: process.env.TKPORT || 80,
+  path: process.env.TKPATH || '/cgi-bin/xmlcgi.pgm',
+};
+
+const lib = 'NODETKTEST'; const dqName = 'TESTQ';
+
+const { returnTransportsDeprecated } = require('../../../lib/utils');
+
+const transports = returnTransportsDeprecated(opt);
+
+describe('iDataQueue Functional Tests', () => {
+  before('setup library for tests and create DQ', async () => {
+    // eslint-disable-next-line global-require
+    const { DBPool } = require('idb-pconnector');
+
+    const pool = new DBPool({ url: '*LOCAL' }, { incrementSize: 2 });
+
+    const qcmdexec = 'CALL QSYS2.QCMDEXC(?)';
+
+    const createLib = `CRTLIB LIB(${lib}) TYPE(*TEST) TEXT('Used to test Node.js toolkit')`;
+
+    const createDQ = `CRTDTAQ DTAQ(${lib}/${dqName}) MAXLEN(100) AUT(*EXCLUDE) TEXT('TEST DQ FOR NODE TOOLKIT TESTS')`;
+
+    const findLib = 'SELECT SCHEMA_NAME FROM qsys2.sysschemas WHERE SCHEMA_NAME = \'NODETKTEST\'';
+
+    const findDQ = 'SELECT OBJLONGNAME FROM TABLE (QSYS2.OBJECT_STATISTICS(\'NODETKTEST\', \'*DTAQ\')) AS X';
+
+    const libResult = await pool.runSql(findLib);
+
+    const dqResult = await pool.runSql(findDQ);
+
+    if (!libResult.length) {
+      await pool.prepareExecute(qcmdexec, [createLib]).catch((error) => {
+        // eslint-disable-next-line no-console
+        console.log('Unable to Create Lib!');
+        throw error;
+      });
+      // eslint-disable-next-line no-console
+      console.log('CREATED LIB!');
+    }
+    if (!dqResult.length) {
+      await pool.prepareExecute(qcmdexec, [createDQ]).catch((error) => {
+        // eslint-disable-next-line no-console
+        console.log('Unable to Create DQ!');
+        throw error;
+      });
+      // eslint-disable-next-line no-console
+      console.log('CREATED DQ!');
+    }
+  });
+  describe('constructor', () => {
+    it('creates and returns an instance of iDataQueue', () => {
+      const connection = new iConn(opt.database, opt.user, opt.password);
+
+      const dq = new iDataQueue(connection);
+      expect(dq).to.be.instanceOf(iDataQueue);
+    });
+  });
+
+  describe('sendToDataQueue', () => {
+    transports.forEach((transport) => {
+      it(`sends data to specified DQ using ${transport.name} transport`, (done) => {
+        const connection = transport.me;
+
+        const dq = new iDataQueue(connection);
+
+        dq.sendToDataQueue(dqName, lib, 'Hello from DQ!', (output) => {
+          expect(output).to.equal(true);
+          done();
+        });
+      });
+    });
+  });
+
+  describe('receiveFromDataQueue', () => {
+    transports.forEach((transport) => {
+      it(`receives data from specfied DQ using ${transport.name} transport`, (done) => {
+        const connection = transport.me;
+
+        const dq = new iDataQueue(connection);
+
+        dq.receiveFromDataQueue(dqName, lib, 100, (output) => {
+          expect(output).to.be.a('string').and.to.equal('Hello from DQ!');
+          done();
+        });
+      });
+    });
+  });
+
+  describe('clearDataQueue', () => {
+    transports.forEach((transport) => {
+      it(`clears the specifed DQ using ${transport.name} transport`, (done) => {
+        const connection = transport.me;
+
+        const dq = new iDataQueue(connection);
+
+        dq.clearDataQueue(dqName, lib, (output) => {
+          expect(output).to.equal(true);
+          done();
+        });
+      });
+    });
+  });
+});
---
+++ test/unit/deprecated/iPgmUnit.js
@@ -0,0 +1,208 @@
+// Copyright (c) International Business Machines Corp. 2019
+// All Rights Reserved
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+// associated documentation files (the "Software"), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify, merge, publish, distribute,
+// sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+// NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+/* eslint-env mocha */
+/* eslint-disable new-cap */
+
+const { expect } = require('chai');
+const { iPgm } = require('../../../lib/itoolkit');
+
+const outBuf = [
+  [0, '10i0'],
+  [0, '10i0'],
+  ['', '36h'],
+  ['', '10A'],
+  ['', '1A'],
+  ['', '1A'],
+  [0, '10i0'],
+  [0, '10i0'],
+];
+const errno = [
+  [0, '10i0'],
+  [0, '10i0', { setlen: 'rec2' }],
+  ['', '7A'],
+  ['', '1A'],
+];
+
+describe('iPgm Class Unit Tests', () => {
+  describe('constructor', () => {
+    it('creates and returns an instance of iPgm with lib and function set', () => {
+      const pgm = new iPgm('QTOCNETSTS');
+      expect(pgm).to.be.instanceOf(iPgm);
+    });
+  });
+
+  describe('toXML', () => {
+    it('returns pgm XML', () => {
+      const pgm = new iPgm('QTOCNETSTS',
+        {
+          lib: 'QSYS',
+          func: 'QtoRtvTCPA',
+          error: 'on',
+        });
+
+      const expectedXML = '<pgm name=\'QTOCNETSTS\' lib=\'QSYS\' func=\'QtoRtvTCPA\' error=\'on\'></pgm>';
+
+      expect(pgm.toXML()).to.be.a('string').and.to.equal(expectedXML);
+    });
+  });
+
+  describe('addParam', () => {
+    it('appends param to pgm xml', () => {
+      const pgm = new iPgm('QTOCNETSTS',
+        {
+          lib: 'QSYS',
+          func: 'QtoRtvTCPA',
+          error: 'fast',
+        });
+
+      pgm.addParam(outBuf, { io: 'out' });
+
+      let expectedXML = '<pgm name=\'QTOCNETSTS\' lib=\'QSYS\' func=\'QtoRtvTCPA\' error=\'fast\'>'
+      + '<parm io=\'out\'><ds><data type=\'10i0\'>0</data><data type=\'10i0\'>'
+      + '0</data><data type=\'36h\'></data><data type=\'10A\'></data>'
+      + '<data type=\'1A\'></data><data type=\'1A\'></data><data type=\'10i0\'>'
+      + '0</data><data type=\'10i0\'>0</data></ds></parm></pgm>';
+
+      expect(pgm.toXML()).to.equal(expectedXML);
+
+      pgm.addParam(66, '10i0');
+
+      expectedXML = '<pgm name=\'QTOCNETSTS\' lib=\'QSYS\' func=\'QtoRtvTCPA\' error=\'fast\'>'
+      + '<parm io=\'out\'><ds><data type=\'10i0\'>0</data><data type=\'10i0\'>'
+      + '0</data><data type=\'36h\'></data><data type=\'10A\'></data>'
+      + '<data type=\'1A\'></data><data type=\'1A\'></data><data type=\'10i0\'>'
+      + '0</data><data type=\'10i0\'>0</data></ds></parm>'
+      + '<parm><data type=\'10i0\'>66</data></parm></pgm>';
+
+      expect(pgm.toXML()).to.equal(expectedXML);
+
+      pgm.addParam(1, '10i0');
+
+      expectedXML = '<pgm name=\'QTOCNETSTS\' lib=\'QSYS\' func=\'QtoRtvTCPA\' error=\'fast\'>'
+      + '<parm io=\'out\'><ds><data type=\'10i0\'>0</data><data type=\'10i0\'>'
+      + '0</data><data type=\'36h\'></data><data type=\'10A\'></data>'
+      + '<data type=\'1A\'></data><data type=\'1A\'></data><data type=\'10i0\'>'
+      + '0</data><data type=\'10i0\'>0</data></ds></parm>'
+      + '<parm><data type=\'10i0\'>66</data></parm>'
+      + '<parm><data type=\'10i0\'>1</data></parm></pgm>';
+
+      expect(pgm.toXML()).to.equal(expectedXML);
+
+      pgm.addParam('QCCSID', '10A');
+
+      expectedXML = '<pgm name=\'QTOCNETSTS\' lib=\'QSYS\' func=\'QtoRtvTCPA\' error=\'fast\'>'
+      + '<parm io=\'out\'><ds><data type=\'10i0\'>0</data><data type=\'10i0\'>'
+      + '0</data><data type=\'36h\'></data><data type=\'10A\'></data>'
+      + '<data type=\'1A\'></data><data type=\'1A\'></data><data type=\'10i0\'>'
+      + '0</data><data type=\'10i0\'>0</data></ds></parm>'
+      + '<parm><data type=\'10i0\'>66</data></parm>'
+      + '<parm><data type=\'10i0\'>1</data></parm>'
+      + '<parm><data type=\'10A\'>QCCSID</data></parm></pgm>';
+
+      expect(pgm.toXML()).to.equal(expectedXML);
+
+      pgm.addParam(errno, { io: 'both', len: 'rec2' });
+
+      expectedXML = '<pgm name=\'QTOCNETSTS\' lib=\'QSYS\' func=\'QtoRtvTCPA\' error=\'fast\'>'
+      + '<parm io=\'out\'><ds><data type=\'10i0\'>0</data><data type=\'10i0\'>'
+      + '0</data><data type=\'36h\'></data><data type=\'10A\'></data>'
+      + '<data type=\'1A\'></data><data type=\'1A\'></data><data type=\'10i0\'>'
+      + '0</data><data type=\'10i0\'>0</data></ds></parm>'
+      + '<parm><data type=\'10i0\'>66</data></parm>'
+      + '<parm><data type=\'10i0\'>1</data></parm>'
+      + '<parm><data type=\'10A\'>QCCSID</data></parm>'
+      + '<parm io=\'both\'><ds len=\'rec2\'><data type=\'10i0\'>0</data>'
+      + '<data type=\'10i0\' setlen=\'rec2\'>0</data><data type=\'7A\'>'
+      + '</data><data type=\'1A\'></data></ds></parm></pgm>';
+
+      expect(pgm.toXML()).to.equal(expectedXML);
+    });
+
+    it('regular <parm> contains by=\'val\'', () => {
+      const pgm = new iPgm('MYPGM', { lib: 'MYLIB', func: 'MY_PROCEDURE' });
+
+      pgm.addParam('', '1A', { by: 'val' });
+      pgm.addReturn('', '2A', { name: 'output' });
+
+      const lookAtXML = pgm.toXML();
+      expect(lookAtXML).to.match(/<parm .*by='val'.*>/);
+    });
+
+    it('data structure <parm> contains by=\'val\'', () => {
+      const pgm = new iPgm('MYPGM', { lib: 'MYLIB', func: 'MY_PROCEDURE' });
+
+      const params = [
+        [0, '3s0'],
+        [0, '7s0', { name: 'ds_fld2' }],
+      ];
+
+      pgm.addParam(params, { name: 'inds', by: 'val' });
+      pgm.addReturn('', '2A', { name: 'output' });
+
+      const lookAtXML = pgm.toXML();
+      expect(lookAtXML).to.match(/<parm .*by='val'.*>/);
+    });
+
+    it('regular <parm> contains by=\'val\', with io=\'both\'', () => {
+      const pgm = new iPgm('MYPGM', { lib: 'MYLIB', func: 'MY_PROCEDURE' });
+
+      pgm.addParam('', '1A', { by: 'val', io: 'both' });
+      pgm.addReturn('', '2A', { name: 'output' });
+
+      const lookAtXML = pgm.toXML();
+      expect(lookAtXML).to.match(/<parm .*by='val'.*>/);
+      expect(lookAtXML).to.match(/<parm .*io='both'.*>/);
+    });
+
+    it('data structure <parm> contains by=\'val\', with io=\'both\'', () => {
+      const pgm = new iPgm('MYPGM', { lib: 'MYLIB', func: 'MY_PROCEDURE' });
+
+      const params = [
+        [0, '3s0'],
+        [0, '7s0', { name: 'ds_fld2' }],
+      ];
+
+      pgm.addParam(params, { name: 'inds', by: 'val', io: 'both' });
+      pgm.addReturn('', '2A', { name: 'output' });
+
+      const lookAtXML = pgm.toXML();
+      expect(lookAtXML).to.match(/<parm .*by='val'.*>/);
+      expect(lookAtXML).to.match(/<parm .*io='both'.*>/);
+    });
+  });
+
+
+  describe('addReturn', () => {
+    it('appends return to pgm xml', () => {
+      const pgm = new iPgm('QTOCNETSTS',
+        {
+          lib: 'QSYS',
+          func: 'QtoRtvTCPA',
+          error: 'fast',
+        });
+
+      pgm.addReturn('0', '20A');
+
+      const expectedXML = '<pgm name=\'QTOCNETSTS\' lib=\'QSYS\' func=\'QtoRtvTCPA\' '
+      + 'error=\'fast\'><return><data type=\'20A\'>0</data></return></pgm>';
+
+      expect(pgm.toXML()).to.equal(expectedXML);
+    });
+  });
+});
