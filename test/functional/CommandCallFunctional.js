4ba0b8f1c8a278b8d948bd841bf6fe3ddb9c1172
Abdirahim Musse <33973272+abmusse@users.noreply.github.com>
2019-04-19 10:45:54 -0500

    [tests] Update configuration options in functional tests
    - Read PK within test files as needed
    - Add missing errno param to ProgramCall functions

--- test/functional/CommandCallFunctional.js
+++ test/functional/CommandCallFunctional.js
@@ -19,17 +19,25 @@
 /* eslint-env mocha */
 
 const { expect } = require('chai');
+const { readFileSync } = require('fs');
 const { CommandCall } = require('../../lib/itoolkit');
 const { xmlToJson, returnTransports } = require('../../lib/utils');
 
 // Set Env variables or set values here.
+let privateKey;
+if (process.env.TKPK) {
+  privateKey = readFileSync(process.env.TKPK, 'utf-8');
+}
 const opt = {
   database: process.env.TKDB || '*LOCAL',
   username: process.env.TKUSER || '',
   password: process.env.TKPASS || '',
   host: process.env.TKHOST || 'localhost',
-  port: process.env.TKPORT || 80,
+  port: process.env.TKPORT,
   path: process.env.TKPATH || '/cgi-bin/xmlcgi.pgm',
+  privateKey,
+  passphrase: process.env.TKPHRASE,
+  verbose: !!process.env.TKVERBOSE,
 };
 
 const transports = returnTransports(opt);
