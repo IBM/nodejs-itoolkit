7507b1af601abba75fac40837932002246451213
Abdirahim Musse <33973272+abmusse@users.noreply.github.com>
2020-02-24 10:05:43 -0600

    refactor: Move SqlCallUnit into iSqlUnit

--- test/unit/deprecated/iSqlUnit.js
+++ test/unit/deprecated/iSqlUnit.js
@@ -40,7 +40,7 @@ describe('iSql Class Unit Tests', () => {
     });
   });
   describe('addQuery', () => {
-    it('appends query to sql XML', () => {
+    it('appends query with error is on to sql XML', () => {
       const sql = new iSql();
 
       const expectedXML = '<sql><query error=\'on\'>select * from QIWS.QCUSTCDT</query></sql>';
@@ -49,24 +49,81 @@ describe('iSql Class Unit Tests', () => {
 
       expect(sql.toXML()).to.equal(expectedXML);
     });
-  });
-  describe('fetch', () => {
-    it('appends fetch to sql XML', () => {
+    it('appends query with options object to sql XML', () => {
       const sql = new iSql();
 
-      const expectedXML = '<sql><fetch block=\'all\' desc=\'on\'></fetch></sql>';
+      const expectedXML = '<sql><query error=\'fast\'>select * from QIWS.QCUSTCDT</query></sql>';
+
+      sql.addQuery('select * from QIWS.QCUSTCDT');
+
+      expect(sql.toXML()).to.equal(expectedXML);
+    });
+  });
+  describe('fetch', () => {
+    it('appends fetch without options object to sql XML', () => {
+      const sql = new iSql();
+
+      const expectedXML = '<sql><fetch block=\'all\' desc=\'on\' error=\'fast\'></fetch></sql>';
 
       sql.fetch();
       expect(sql.toXML()).to.equal(expectedXML);
     });
-  });
-  describe('commit', () => {
-    it('appends commit to sql XML', () => {
+    it('appends fetch with block is 10 to sql XML', () => {
       const sql = new iSql();
 
-      const expectedXML = '<sql><commit action=\'commit\'></commit></sql>';
+      const expectedXML = '<sql><fetch block=\'10\' desc=\'on\' error=\'fast\'></fetch></sql>';
+
+      sql.fetch({ block: '10' });
+      expect(sql.toXML()).to.equal(expectedXML);
+    });
+    it('appends fetch with desc is off to sql XML', () => {
+      const sql = new iSql();
+
+      const expectedXML = '<sql><fetch block=\'all\' desc=\'off\' error=\'fast\'></fetch></sql>';
+
+      sql.fetch({ desc: 'off' });
+      expect(sql.toXML()).to.equal(expectedXML);
+    });
+    it('appends fetch with error is on to sql XML', () => {
+      const sql = new iSql();
+
+      const expectedXML = '<sql><fetch block=\'all\' desc=\'on\' error=\'on\'></fetch></sql>';
+
+      sql.fetch({ error: 'on' });
+      expect(sql.toXML()).to.equal(expectedXML);
+    });
+  });
+  describe('commit', () => {
+    it('appends commit with action commit to sql XML', () => {
+      const sql = new iSql();
+
+      const expectedXML = '<sql><commit action=\'commit\' error=\'fast\'></commit></sql>';
 
       sql.commit({ action: 'commit' });
+      expect(sql.toXML()).to.equal(expectedXML);
+    });
+    it('appends commit with action rollback to sql XML', () => {
+      const sql = new iSql();
+
+      const expectedXML = '<sql><commit action=\'rollback\' error=\'fast\'></commit></sql>';
+
+      sql.commit({ action: 'rollback' });
+      expect(sql.toXML()).to.equal(expectedXML);
+    });
+    it('appends commit with default action to sql XML', () => {
+      const sql = new iSql();
+
+      const expectedXML = '<sql><commit action=\'commit\' error=\'fast\'></commit></sql>';
+
+      sql.commit();
+      expect(sql.toXML()).to.equal(expectedXML);
+    });
+    it('appends commit with error is on to sql XML', () => {
+      const sql = new iSql();
+
+      const expectedXML = '<sql><commit action=\'commit\' error=\'on\'></commit></sql>';
+
+      sql.commit({ action: 'commit', error: 'on' });
       expect(sql.toXML()).to.equal(expectedXML);
     });
   });
@@ -79,10 +136,18 @@ describe('iSql Class Unit Tests', () => {
       sql.prepare('SELECT * FROM QIWS.QCUSTCDT WHERE BALDUE > ?');
       expect(sql.toXML()).to.equal(expectedXML);
     });
+    it('appends prepare with error on to sql XML', () => {
+      const sql = new iSql();
+
+      const expectedXML = '<sql><prepare error=\'on\'>SELECT * FROM QIWS.QCUSTCDT WHERE BALDUE > ?</prepare></sql>';
+
+      sql.prepare('SELECT * FROM QIWS.QCUSTCDT WHERE BALDUE > ?', { error: 'on' });
+      expect(sql.toXML()).to.equal(expectedXML);
+    });
   });
 
   describe('execute', () => {
-    it('appends execute to sql XML', () => {
+    it('appends execute with parameters to sql XML', () => {
       const sql = new iSql();
 
       const expectedXML = '<sql><execute error=\'fast\'><parm io=\'in\'>30</parm></execute></sql>';
@@ -90,14 +155,46 @@ describe('iSql Class Unit Tests', () => {
       sql.execute([[30, 'in']]);
       expect(sql.toXML()).to.equal(expectedXML);
     });
+    it('appends execute with parameters and error on to sql XML', () => {
+      const sql = new iSql();
+
+      const expectedXML = '<sql><execute error=\'on\'><parm io=\'in\'>30</parm></execute></sql>';
+
+      sql.execute([[30, 'in']], { error: 'on' });
+      expect(sql.toXML()).to.equal(expectedXML);
+    });
+    it('appends execute without parameters to sql XML', () => {
+      const sql = new iSql();
+
+      const expectedXML = '<sql><execute error=\'fast\'></execute></sql>';
+
+      sql.execute();
+      expect(sql.toXML()).to.equal(expectedXML);
+    });
+    it('appends execute without parameters and error on to sql XML', () => {
+      const sql = new iSql();
+
+      const expectedXML = '<sql><execute error=\'on\'></execute></sql>';
+
+      sql.execute(null, { error: 'on' });
+      expect(sql.toXML()).to.equal(expectedXML);
+    });
   });
   describe('tables', () => {
     it('appends tables to sql XML', () => {
       const sql = new iSql();
 
-      const expectedXML = '<sql><tables><parm></parm><parm>QIWS</parm><parm></parm><parm></parm></tables></sql>';
+      const expectedXML = '<sql><tables error=\'fast\'><parm></parm><parm>QIWS</parm><parm></parm><parm></parm></tables></sql>';
       // catalog, schema, table, table type
       sql.tables(['', 'QIWS', '', '']);
+      expect(sql.toXML()).to.equal(expectedXML);
+    });
+    it('appends tables with error on to sql XML', () => {
+      const sql = new iSql();
+
+      const expectedXML = '<sql><tables error=\'on\'><parm></parm><parm>QIWS</parm><parm></parm><parm></parm></tables></sql>';
+      // catalog, schema, table, table type
+      sql.tables(['', 'QIWS', '', ''], { error: 'on' });
       expect(sql.toXML()).to.equal(expectedXML);
     });
   });
@@ -105,9 +202,17 @@ describe('iSql Class Unit Tests', () => {
     it('appends tablepriv to sql XML', () => {
       const sql = new iSql();
 
-      const expectedXML = '<sql><tablepriv><parm></parm><parm>QIWS</parm><parm>QCUSTCDT</parm></tablepriv></sql>';
+      const expectedXML = '<sql><tablepriv error=\'fast\'><parm></parm><parm>QIWS</parm><parm>QCUSTCDT</parm></tablepriv></sql>';
       // catalog, schema, table
       sql.tablePriv(['', 'QIWS', 'QCUSTCDT']);
+      expect(sql.toXML()).to.equal(expectedXML);
+    });
+    it('appends tablepriv with error on to sql XML', () => {
+      const sql = new iSql();
+
+      const expectedXML = '<sql><tablepriv error=\'on\'><parm></parm><parm>QIWS</parm><parm>QCUSTCDT</parm></tablepriv></sql>';
+      // catalog, schema, table
+      sql.tablePriv(['', 'QIWS', 'QCUSTCDT'], { error: 'on' });
       expect(sql.toXML()).to.equal(expectedXML);
     });
   });
@@ -115,9 +220,17 @@ describe('iSql Class Unit Tests', () => {
     it('appends columns to sql XML', () => {
       const sql = new iSql();
 
-      const expectedXML = '<sql><columns><parm></parm><parm>QIWS</parm><parm>QCUSTCDT</parm><parm></parm></columns></sql>';
+      const expectedXML = '<sql><columns error=\'fast\'><parm></parm><parm>QIWS</parm><parm>QCUSTCDT</parm><parm></parm></columns></sql>';
       // catalog, schema, table, column
       sql.columns(['', 'QIWS', 'QCUSTCDT', '']);
+      expect(sql.toXML()).to.equal(expectedXML);
+    });
+    it('appends columns with error on to sql XML', () => {
+      const sql = new iSql();
+
+      const expectedXML = '<sql><columns error=\'on\'><parm></parm><parm>QIWS</parm><parm>QCUSTCDT</parm><parm></parm></columns></sql>';
+      // catalog, schema, table, column
+      sql.columns(['', 'QIWS', 'QCUSTCDT', ''], { error: 'on' });
       expect(sql.toXML()).to.equal(expectedXML);
     });
   });
@@ -125,9 +238,17 @@ describe('iSql Class Unit Tests', () => {
     it('appends columnpriv to sql XML', () => {
       const sql = new iSql();
 
-      const expectedXML = '<sql><columnpriv><parm></parm><parm>QIWS</parm><parm>QCUSTCDT</parm><parm></parm></columnpriv></sql>';
+      const expectedXML = '<sql><columnpriv error=\'fast\'><parm></parm><parm>QIWS</parm><parm>QCUSTCDT</parm><parm></parm></columnpriv></sql>';
       // catalog, schema, table, column
       sql.columnPriv(['', 'QIWS', 'QCUSTCDT', '']);
+      expect(sql.toXML()).to.equal(expectedXML);
+    });
+    it('appends columnpriv with error on to sql XML', () => {
+      const sql = new iSql();
+
+      const expectedXML = '<sql><columnpriv error=\'on\'><parm></parm><parm>QIWS</parm><parm>QCUSTCDT</parm><parm></parm></columnpriv></sql>';
+      // catalog, schema, table, column
+      sql.columnPriv(['', 'QIWS', 'QCUSTCDT', ''], { error: 'on' });
       expect(sql.toXML()).to.equal(expectedXML);
     });
   });
@@ -135,21 +256,37 @@ describe('iSql Class Unit Tests', () => {
     it('appends procedures to sql XML', () => {
       const sql = new iSql();
 
-      const expectedXML = '<sql><procedures><parm></parm><parm>QSYS2</parm><parm>TCPIP_INFO</parm></procedures></sql>';
+      const expectedXML = '<sql><procedures error=\'fast\'><parm></parm><parm>QSYS2</parm><parm>TCPIP_INFO</parm></procedures></sql>';
       // catalog, schema, procedure
       sql.procedures(['', 'QSYS2', 'TCPIP_INFO']);
+      expect(sql.toXML()).to.equal(expectedXML);
+    });
+    it('appends procedures with error on to sql XML', () => {
+      const sql = new iSql();
+
+      const expectedXML = '<sql><procedures error=\'on\'><parm></parm><parm>QSYS2</parm><parm>TCPIP_INFO</parm></procedures></sql>';
+      // catalog, schema, procedure
+      sql.procedures(['', 'QSYS2', 'TCPIP_INFO'], { error: 'on' });
       expect(sql.toXML()).to.equal(expectedXML);
     });
   });
   describe('pColumns', () => {
     it('appends pColumns to sql XML', () => {
       // procedure columns:
-
       const sql = new iSql();
 
-      const expectedXML = '<sql><pcolumns><parm></parm><parm>QSYS2</parm><parm>QCMDEXC</parm><parm>COMMAND</parm></pcolumns></sql>';
+      const expectedXML = '<sql><pcolumns error=\'fast\'><parm></parm><parm>QSYS2</parm><parm>QCMDEXC</parm><parm>COMMAND</parm></pcolumns></sql>';
       // catalog, schema, procedure, column
       sql.pColumns(['', 'QSYS2', 'QCMDEXC', 'COMMAND']);
+      expect(sql.toXML()).to.equal(expectedXML);
+    });
+    it('appends pColumns with error on to sql XML', () => {
+      // procedure columns:
+      const sql = new iSql();
+
+      const expectedXML = '<sql><pcolumns error=\'on\'><parm></parm><parm>QSYS2</parm><parm>QCMDEXC</parm><parm>COMMAND</parm></pcolumns></sql>';
+      // catalog, schema, procedure, column
+      sql.pColumns(['', 'QSYS2', 'QCMDEXC', 'COMMAND'], { error: 'on' });
       expect(sql.toXML()).to.equal(expectedXML);
     });
   });
@@ -157,9 +294,17 @@ describe('iSql Class Unit Tests', () => {
     it('appends primarykeys to sql XML', () => {
       const sql = new iSql();
 
-      const expectedXML = '<sql><primarykeys><parm></parm><parm>QUSRSYS</parm><parm>QASZRAIRX</parm></primarykeys></sql>';
+      const expectedXML = '<sql><primarykeys error=\'fast\'><parm></parm><parm>QUSRSYS</parm><parm>QASZRAIRX</parm></primarykeys></sql>';
       // catalog, schema, table
       sql.primaryKeys(['', 'QUSRSYS', 'QASZRAIRX']);
+      expect(sql.toXML()).to.equal(expectedXML);
+    });
+    it('appends primarykeys with error on to sql XML', () => {
+      const sql = new iSql();
+
+      const expectedXML = '<sql><primarykeys error=\'on\'><parm></parm><parm>QUSRSYS</parm><parm>QASZRAIRX</parm></primarykeys></sql>';
+      // catalog, schema, table
+      sql.primaryKeys(['', 'QUSRSYS', 'QASZRAIRX'], { error: 'on' });
       expect(sql.toXML()).to.equal(expectedXML);
     });
   });
@@ -167,11 +312,21 @@ describe('iSql Class Unit Tests', () => {
     it('appends foreignkeys to sql XML', () => {
       const sql = new iSql();
 
-      const expectedXML = '<sql><foreignkeys><parm></parm><parm>QUSRSYS</parm><parm>QASZRAIRC</parm><parm></parm><parm>QUSRSYS</parm><parm>QASZRAIRX</parm></foreignkeys></sql>';
+      const expectedXML = '<sql><foreignkeys error=\'fast\'><parm></parm><parm>QUSRSYS</parm><parm>QASZRAIRC</parm><parm></parm><parm>QUSRSYS</parm><parm>QASZRAIRX</parm></foreignkeys></sql>';
 
       // pk: catalog, schema, table
       // fk: catalog, schema, table
       sql.foreignKeys(['', 'QUSRSYS', 'QASZRAIRC', '', 'QUSRSYS', 'QASZRAIRX']);
+      expect(sql.toXML()).to.equal(expectedXML);
+    });
+    it('appends foreignkeys with error on to sql XML', () => {
+      const sql = new iSql();
+
+      const expectedXML = '<sql><foreignkeys error=\'on\'><parm></parm><parm>QUSRSYS</parm><parm>QASZRAIRC</parm><parm></parm><parm>QUSRSYS</parm><parm>QASZRAIRX</parm></foreignkeys></sql>';
+
+      // pk: catalog, schema, table
+      // fk: catalog, schema, table
+      sql.foreignKeys(['', 'QUSRSYS', 'QASZRAIRC', '', 'QUSRSYS', 'QASZRAIRX'], { error: 'on' });
       expect(sql.toXML()).to.equal(expectedXML);
     });
   });
@@ -179,9 +334,17 @@ describe('iSql Class Unit Tests', () => {
     it('appends statistics to sql XML', () => {
       const sql = new iSql();
 
-      const expectedXML = '<sql><statistics><parm></parm><parm>QIWS</parm><parm>QCUSTCDT</parm><parm>all</parm></statistics></sql>';
+      const expectedXML = '<sql><statistics error=\'fast\'><parm></parm><parm>QIWS</parm><parm>QCUSTCDT</parm><parm>all</parm></statistics></sql>';
       // catalog, schema, table, all | unique
       sql.statistics(['', 'QIWS', 'QCUSTCDT', 'all']);
+      expect(sql.toXML()).to.equal(expectedXML);
+    });
+    it('appends statistics with error on to sql XML', () => {
+      const sql = new iSql();
+
+      const expectedXML = '<sql><statistics error=\'on\'><parm></parm><parm>QIWS</parm><parm>QCUSTCDT</parm><parm>all</parm></statistics></sql>';
+      // catalog, schema, table, all | unique
+      sql.statistics(['', 'QIWS', 'QCUSTCDT', 'all'], { error: 'on' });
       expect(sql.toXML()).to.equal(expectedXML);
     });
   });
@@ -189,9 +352,17 @@ describe('iSql Class Unit Tests', () => {
     it('appends special to sql XML', () => {
       const sql = new iSql();
 
-      const expectedXML = '<sql><special><parm></parm><parm>QIWS</parm><parm>QCUSTCDT</parm><parm>row</parm><parm>no</parm></special></sql>';
+      const expectedXML = '<sql><special error=\'fast\'><parm></parm><parm>QIWS</parm><parm>QCUSTCDT</parm><parm>row</parm><parm>no</parm></special></sql>';
       // catalog, schema, table, row | transaction | session, no | unique
       sql.special(['', 'QIWS', 'QCUSTCDT', 'row', 'no']);
+      expect(sql.toXML()).to.equal(expectedXML);
+    });
+    it('appends special with error on to sql XML', () => {
+      const sql = new iSql();
+
+      const expectedXML = '<sql><special error=\'on\'><parm></parm><parm>QIWS</parm><parm>QCUSTCDT</parm><parm>row</parm><parm>no</parm></special></sql>';
+      // catalog, schema, table, row | transaction | session, no | unique
+      sql.special(['', 'QIWS', 'QCUSTCDT', 'row', 'no'], { error: 'on' });
       expect(sql.toXML()).to.equal(expectedXML);
     });
   });
@@ -199,9 +370,22 @@ describe('iSql Class Unit Tests', () => {
     it('appends count to sql XML', () => {
       const sql = new iSql();
 
-      const expectedXML = '<sql><count desc=\'both\'></count></sql>';
-      // catalog, schema, table, all | unique
+      const expectedXML = '<sql><count desc=\'both\' error=\'fast\'></count></sql>';
       sql.count({ desc: 'both' });
+      expect(sql.toXML()).to.equal(expectedXML);
+    });
+    it('appends count without options object to sql XML', () => {
+      const sql = new iSql();
+
+      const expectedXML = '<sql><count desc=\'both\' error=\'fast\'></count></sql>';
+      sql.count();
+      expect(sql.toXML()).to.equal(expectedXML);
+    });
+    it('appends count with error on to sql XML', () => {
+      const sql = new iSql();
+
+      const expectedXML = '<sql><count desc=\'both\' error=\'on\'></count></sql>';
+      sql.count({ error: 'on' });
       expect(sql.toXML()).to.equal(expectedXML);
     });
   });
@@ -209,9 +393,15 @@ describe('iSql Class Unit Tests', () => {
     it('appends rowcount to sql XML', () => {
       const sql = new iSql();
 
-      const expectedXML = '<sql><rowcount></rowcount></sql>';
-      // catalog, schema, table, all | unique
+      const expectedXML = '<sql><rowcount error=\'fast\'></rowcount></sql>';
       sql.rowCount();
+      expect(sql.toXML()).to.equal(expectedXML);
+    });
+    it('appends rowcount with error on to sql XML', () => {
+      const sql = new iSql();
+
+      const expectedXML = '<sql><rowcount error=\'on\'></rowcount></sql>';
+      sql.rowCount({ error: 'on' });
       expect(sql.toXML()).to.equal(expectedXML);
     });
   });
@@ -220,7 +410,6 @@ describe('iSql Class Unit Tests', () => {
       const sql = new iSql();
 
       const expectedXML = '<sql><free></free></sql>';
-      // catalog, schema, table, all | unique
       sql.free();
       expect(sql.toXML()).to.equal(expectedXML);
     });
@@ -229,9 +418,25 @@ describe('iSql Class Unit Tests', () => {
     it('appends describe to sql XML', () => {
       const sql = new iSql();
 
-      const expectedXML = '<sql><describe desc=\'both\'></describe></sql>';
+      const expectedXML = '<sql><describe desc=\'both\' error=\'fast\'></describe></sql>';
 
       sql.describe({ desc: 'both' });
+      expect(sql.toXML()).to.equal(expectedXML);
+    });
+    it('appends describe without options object to sql XML', () => {
+      const sql = new iSql();
+
+      const expectedXML = '<sql><describe desc=\'both\' error=\'fast\'></describe></sql>';
+
+      sql.describe();
+      expect(sql.toXML()).to.equal(expectedXML);
+    });
+    it('appends describe with error on to sql XML', () => {
+      const sql = new iSql();
+
+      const expectedXML = '<sql><describe desc=\'both\' error=\'on\'></describe></sql>';
+
+      sql.describe({ error: 'on' });
       expect(sql.toXML()).to.equal(expectedXML);
     });
   });
