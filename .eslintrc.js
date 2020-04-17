61fbc3f5e1c6927cb1b29bae003a08e1df058690
Kevin Adler <kadler@us.ibm.com>
2020-04-17 10:43:40 -0500

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports =  {
         // Tweak rules set by airbnb config
         'no-console': 'off', // allow console
         'import/no-extraneous-dependencies': ["error", {"optionalDependencies": true}], // Allow optional dep import
-        // Warn unreoslved imports ie: idb-connector import will be unresolved when on non IBM i system.
+        // Warn unresolved imports ie: idb-connector import will be unresolved when on non IBM i system.
         'import/no-unresolved': ['off', { commonjs: true }],
       },
 };
