40eb4590b6a2e83ebc9d52ce2333ab9c9c0fe4af
Kevin Adler <kadler@us.ibm.com>
2020-06-10 16:33:56 -0500

    test: Fix eslint errors issues in eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,11 @@
-module.exports =  { 
-    "extends": "airbnb-base",
-    env: {
-        node: true
-    },
-    rules: {
-        // Tweak rules set by airbnb config
-        // We need to allow use of console.log for verbose mode
-        'no-console': 'off',
-      },
+module.exports = {
+  extends: 'airbnb-base',
+  env: {
+    node: true,
+  },
+  rules: {
+    // Tweak rules set by airbnb config
+    // We need to allow use of console.log for verbose mode
+    'no-console': 'off',
+  },
 };
